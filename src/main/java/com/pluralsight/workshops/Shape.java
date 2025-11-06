c3a49d1bf07103d5eb1328532b6fad867390840b
Prince Haywood <praytoo52@icloud.com>
2025-11-06 11:02:39 -0800

    update

---
+++ src/main/java/com/pluralsight/workshops/Shape.java
@@ -0,0 +1,14 @@
+package com.pluralsight.workshops;
+
+import java.awt.*;
+
+public abstract class Shape {
+    private Turtle turtle;
+    private Point location;
+    private Color color;
+    private int border;
+
+    public void paint(){
+
+    }
+}
